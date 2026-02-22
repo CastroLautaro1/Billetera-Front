5be68467a7bfcbb3c249b109201e6e3ef3f25ebc
Castro Lautaro <castrolautaro235@gmail.com>
2026-02-22 15:52:14 -0300

    feat: add services in home component to get data from account and transactions

--- src/app/features/home-component/home-component.ts
+++ src/app/features/home-component/home-component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { AccountService } from '../../core/services/account-service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { UserService } from '../../core/services/user-service';
+import { TransactionService } from '../../core/services/transaction-service';
+import { MatMenuTrigger } from "../../../../node_modules/@angular/material/menu/index";
+import { DatePipe } from '@angular/common';
+import { map, tap } from 'rxjs';
+import { UserFullnameComponent } from '../../shared/components/user-fullname-component/user-fullname-component';
 
 @Component({
   selector: 'app-home-component',
-  imports: [],
+  imports: [MatIcon, DatePipe, UserFullnameComponent],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
 })
 export class HomeComponent {
+  private _account = inject(AccountService);
+  private _user = inject(UserService);
+  private _transaction = inject(TransactionService);
+  userAccount = toSignal(
+    this._account.getProfile(),
+    {initialValue : undefined}
+  );
+
+  userData = toSignal(
+    this._user.getProfile(),
+    {initialValue : undefined}
+  )
+
+  transactionHistory = toSignal(
+    this._transaction.getHistory(0, 6),
+    {initialValue : undefined}
+  )
 
 }
