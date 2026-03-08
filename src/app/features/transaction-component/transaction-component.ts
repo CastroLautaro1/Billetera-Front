a6242c4fbe2113cf40175f6f2d525a71a0ad0130
Castro Lautaro <castrolautaro235@gmail.com>
2026-03-08 17:53:05 -0300

    feat: add logic to download a receipt

--- src/app/features/transaction-component/transaction-component.ts
+++ src/app/features/transaction-component/transaction-component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, LOCALE_ID } from '@angular/core';
 import { AccountService } from '../../core/services/account-service';
 import { ActivatedRoute } from '@angular/router';
 import { filter, map, switchMap } from 'rxjs';
@@ -6,10 +6,14 @@ import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { TransactionService } from '../../core/services/transaction-service';
 import { MatIcon } from '@angular/material/icon';
 import { DatePipe } from '@angular/common';
+import { FileDownloadService } from '../../core/services/file-download-service';
 
 @Component({
   selector: 'app-transaction-component',
   imports: [MatIcon, DatePipe],
+  providers: [{
+    provide: LOCALE_ID, useValue: 'es-AR'
+  }],
   templateUrl: './transaction-component.html',
   styleUrl: './transaction-component.css',
 })
@@ -17,6 +21,9 @@ export class TransactionComponent {
   private _transaction = inject(TransactionService);
   private _account = inject(AccountService);
   private _route = inject(ActivatedRoute);
+  private _fileDownload = inject(FileDownloadService);
+
+  isDownloading = false;
 
   transactionId$ = this._route.paramMap.pipe(
     map(param => param.get('id')),
@@ -46,5 +53,46 @@ export class TransactionComponent {
     ),
     { initialValue : undefined }
   );
+
+  userAccount = toSignal(
+    this._account.getProfile(),
+    { initialValue : undefined }
+  );
+
+  downloadReceipt() {
+    const txId = this.transaction()?.id;
+    
+    if (!txId) {
+      console.error('No hay ID de transacción para descargar');
+      return;
+    }
+
+    this.isDownloading = true;
+
+    this._transaction.downloadReceiptPdf(txId).subscribe({
+      next: (blob: Blob) => {
+        this._fileDownload.downloadBlobAsFile(blob, `Comprobante_Oran_${txId}.pdf`);
+      },
+      error: (error) => {
+        console.error('Error al descargar el comprobante desde el servidor', error);
+      },
+      complete: () => {
+        this.isDownloading = false;
+      }
+    });
+  }
+
+  formmatedReference(id: number | undefined): string {
+    if (!id) return 'REF-PENDIENTE';
+  
+    const paddedId = id.toString().padStart(8, '0');
+    return `REF-${paddedId}`;
+  }
+
+  isEntry(): boolean {
+    if (this.transaction()?.transactionType === 'DEPOSIT') return true;
+
+    return this.transaction()?.transactionType === 'TRANSFER' && this.transaction()?.counterpartyAccountId === this.userAccount()?.id;
+  }
 
 }
